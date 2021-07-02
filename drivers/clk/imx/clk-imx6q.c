c83c497137bb184b2f39112521f3d8d0774ab509
Dong Aisheng <aisheng.dong@nxp.com>
2021-07-02 16:54:38 +0800

    clk: imx6q: fix uart earlycon unwork
    The earlycon depends on the bootloader setup UART clocks being retained.
    There're actually two uart clocks (ipg, per) on MX6QDL,
    but the 'Fixes' commit change to register only one which means
    another clock may be disabled during booting phase
    and result in the earlycon unwork.
    
    Cc: stable@vger.kernel.org # v5.10+
    Fixes: 379c9a24cc23 ("clk: imx: Fix reparenting of UARTs not associated with stdout")
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    Link: https://lore.kernel.org/r/20210702085438.1988087-1-aisheng.dong@nxp.com
    Reviewed-by: Abel Vesa <abel.vesa@nxp.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/imx/clk-imx6q.c
+++ drivers/clk/imx/clk-imx6q.c
@@ -982,12 +982,6 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 			       hws[IMX6QDL_CLK_PLL3_USB_OTG]->clk);
 	}
 
-	for (i = 0; i < ARRAY_SIZE(uart_clk_ids); i++) {
-		int index = uart_clk_ids[i];
-
-		uart_clks[i] = &hws[index]->clk;
-	}
-
-	imx_register_uart_clocks(uart_clks);
+	imx_register_uart_clocks(2);
 }
 CLK_OF_DECLARE(imx6q, "fsl,imx6q-ccm", imx6q_clocks_init);
