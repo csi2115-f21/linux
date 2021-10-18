e2ffb79ccf3cd6f3b1df91e13f9bdf5319ae644f
michaelp3 <chan-park@hotmail.com>
2021-10-18 13:53:18 +0900

    resolve conflicts

--- kernel/tracepoint.c
+++ kernel/tracepoint.c
@@ -136,15 +136,9 @@ func_add(struct tracepoint_func **funcs, struct tracepoint_func *tp_func,
 	 int prio)
 {
 	struct tracepoint_func *old, *new;
-<<<<<<< HEAD
-	int iter_probes;	/* Iterate over old probe array. */
-	int nr_probes = 0;	/* Counter for probes */
-	int pos = -1;		/* Insertion position into new array */
-=======
 	int nr_probes = 0;
 	int stub_funcs = 0;
 	int pos = -1;
->>>>>>> befe6d946551... tracepoint: Do not fail unregistering a probe due to memory failure
 
 	if (WARN_ON(!tp_func->func))
 		return ERR_PTR(-EINVAL);
@@ -153,18 +147,15 @@ func_add(struct tracepoint_func **funcs, struct tracepoint_func *tp_func,
 	old = *funcs;
 	if (old) {
 		/* (N -> N+1), (N != 0, 1) probes */
-		for (iter_probes = 0; old[iter_probes].func; iter_probes++) {
-			if (old[iter_probes].func == tp_stub_func)
-				continue;	/* Skip stub functions. */
-			if (old[iter_probes].func == tp_func->func &&
-			    old[iter_probes].data == tp_func->data)
+		for (nr_probes = 0; old[nr_probes].func; nr_probes++) {
+			/* Insert before probes of lower priority */
+			if (pos < 0 && old[nr_probes].prio < prio)
+				pos = nr_probes;
+			if (old[nr_probes].func == tp_func->func &&
+			    old[nr_probes].data == tp_func->data)
 				return ERR_PTR(-EEXIST);
-<<<<<<< HEAD
-			nr_probes++;
-=======
 			if (old[nr_probes].func == tp_stub_func)
 				stub_funcs++;
->>>>>>> befe6d946551... tracepoint: Do not fail unregistering a probe due to memory failure
 		}
 	}
 	/* + 2 : one for new probe, one for NULL func - stub functions */
@@ -172,16 +163,6 @@ func_add(struct tracepoint_func **funcs, struct tracepoint_func *tp_func,
 	if (new == NULL)
 		return ERR_PTR(-ENOMEM);
 	if (old) {
-<<<<<<< HEAD
-		nr_probes = 0;
-		for (iter_probes = 0; old[iter_probes].func; iter_probes++) {
-			if (old[iter_probes].func == tp_stub_func)
-				continue;
-			/* Insert before probes of lower priority */
-			if (pos < 0 && old[iter_probes].prio < prio)
-				pos = nr_probes++;
-			new[nr_probes++] = old[iter_probes];
-=======
 		if (stub_funcs) {
 			/* Need to copy one at a time to remove stubs */
 			int probes = 0;
@@ -209,17 +190,11 @@ func_add(struct tracepoint_func **funcs, struct tracepoint_func *tp_func,
 			/* Copy the rest after it. */
 			memcpy(new + pos + 1, old + pos,
 			       (nr_probes - pos) * sizeof(struct tracepoint_func));
->>>>>>> befe6d946551... tracepoint: Do not fail unregistering a probe due to memory failure
 		}
-		if (pos < 0)
-			pos = nr_probes++;
-		/* nr_probes now points to the end of the new array */
-	} else {
+	} else
 		pos = 0;
-		nr_probes = 1; /* must point at end of array */
-	}
 	new[pos] = *tp_func;
-	new[nr_probes].func = NULL;
+	new[nr_probes + 1].func = NULL;
 	*funcs = new;
 	debug_print_probes(*funcs);
 	return old;
@@ -262,20 +237,11 @@ static void *func_remove(struct tracepoint_func **funcs,
 		/* + 1 for NULL */
 		new = allocate_probes(nr_probes - nr_del + 1);
 		if (new) {
-<<<<<<< HEAD
-			for (i = 0; old[i].func; i++) {
-				if ((old[i].func != tp_func->func ||
-				     old[i].data != tp_func->data) &&
-				    old[i].func != tp_stub_func)
-					new[j++] = old[i];
-			}
-=======
 			for (i = 0; old[i].func; i++)
 				if ((old[i].func != tp_func->func
 				     || old[i].data != tp_func->data)
 				    && old[i].func != tp_stub_func)
 					new[j++] = old[i];
->>>>>>> befe6d946551... tracepoint: Do not fail unregistering a probe due to memory failure
 			new[nr_probes - nr_del].func = NULL;
 			*funcs = new;
 		} else {
@@ -283,13 +249,6 @@ static void *func_remove(struct tracepoint_func **funcs,
 			 * Failed to allocate, replace the old function
 			 * with calls to tp_stub_func.
 			 */
-<<<<<<< HEAD
-			for (i = 0; old[i].func; i++) {
-				if (old[i].func == tp_func->func &&
-				    old[i].data == tp_func->data)
-					WRITE_ONCE(old[i].func, tp_stub_func);
-			}
-=======
 			for (i = 0; old[i].func; i++)
 				if (old[i].func == tp_func->func &&
 				    old[i].data == tp_func->data) {
@@ -301,7 +260,6 @@ static void *func_remove(struct tracepoint_func **funcs,
 					else
 						old[i].prio = -1;
 				}
->>>>>>> befe6d946551... tracepoint: Do not fail unregistering a probe due to memory failure
 			*funcs = old;
 		}
 	}
