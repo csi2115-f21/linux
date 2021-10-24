9b94b2a85c579a701f2638addaee4d01cec955cf
2020147570 <2020147570@yonsei.ac.kr>
2021-10-24 16:56:09 +0900

    Revert "Merge tag 'block-5.15-2021-10-22' of git://git.kernel.dk/linux-block"
    This reverts commit 9c0c4d24ac000e52d55348961d3a3ba42065e0cf, reversing
    changes made to da4d34b669723508601a4c29daa22cdc669ee005.

--- block/partitions/core.c
+++ block/partitions/core.c
@@ -435,7 +435,10 @@ out_put:
 	put_device(pdev);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> parent of 9c0c4d24ac00... Merge tag 'block-5.15-2021-10-22' of git://git.kernel.dk/linux-block
 =======
 >>>>>>> parent of 9c0c4d24ac00... Merge tag 'block-5.15-2021-10-22' of git://git.kernel.dk/linux-block
 out_put_disk:
